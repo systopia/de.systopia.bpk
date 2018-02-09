e7c383c65791e570b4777f4660e2429752e56763
systopia <endres@systopia.de>
2018-02-09 17:51:12 +0100

    added time based limits

--- api/v3/Bpk/Lookup.php
+++ api/v3/Bpk/Lookup.php
@@ -18,6 +18,22 @@
  * BPK Lookup
  */
 function civicrm_api3_bpk_lookup($params) {
+  // process time based limits
+  //  format is 'limit_x-y' (x and y should be times)
+  foreach ($params as $key => $value) {
+    if (substr($key, 0, 6) == 'limit_') {
+      $hours = explode('-', substr($key, 6));
+      if (count($hours) == 2 && is_numeric($hours[0]) && is_numeric($hours[1])) {
+        // this seems to be a correct limit restriction
+        $current_hour = date('H');
+        if ($current_hour >= $hours[0] && $current_hour < $hours[1]) {
+          // ...and it applies
+          $params['limit'] = $value;
+        }
+      }
+    }
+  }
+
   $result = CRM_Bpk_Lookup::doSoapLookup($params);
   return civicrm_api3_create_success("{$result['success']} contacts resolved successfully, {$result['failed']} failures. Contacts (IDs) processed: {$result['contact_ids']}.");
 }
