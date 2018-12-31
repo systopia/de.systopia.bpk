505cd0cccd2210610b6d84f434c7056909f51750
B. Endres <endres@systopia.de>
2018-12-31 10:55:16 +0100

    [#3] implementing activity based exclusion (WIP)

--- CRM/Bpk/Page/SubmissionTab.php
+++ CRM/Bpk/Page/SubmissionTab.php
@@ -90,10 +90,69 @@ class CRM_Bpk_Page_SubmissionTab extends CRM_Core_Page {
 
     $this->assign('submissions', $submissions);
 
-    // let's add some style...
+
+    // add exclusion information
+    // FIRST: exclusion groups
+    $config = CRM_Bpk_Config::singleton();
+    $excluded_group_ids = $config->getGroupsExcludedFromSubmission();
+    if (empty($excluded_group_ids)) {
+      $this->assign('exclusion_group_status', E::ts("No exclusion groups have been configured."));
+
+    } else {
+      // load groups
+      $group_ids = explode(',', $excluded_group_ids);
+      $group_names = [];
+      $groups = civicrm_api3('Group', 'get', [
+          'return'       => 'title,id',
+          'id'           => ['IN' => $group_ids],
+          'option.limit' => 0]);
+      foreach ($groups['values'] as $group) {
+        $group_names[] = $group['title'];
+      }
+      $group_name_list = '"' . implode('", "', $group_names) . '"';
+
+      // check if contact is a member of those groups
+      $membership_count = 0;
+      foreach ($group_ids as $group_id) { // cannot use 'IN' => $group_ids in CiviCRM 4.6x
+        $membership = civicrm_api3('GroupContact', 'get', [
+            'group_id'   => $group_id,
+            'contact_id' => $contact_id]);
+        $membership_count += $membership['count'];
+      }
+
+      if ($membership_count > 0) {
+        $this->assign('exclusion_group_status', E::ts("This contact is member of %2 of the excluding groups (%1). <strong>No contributions will be submitted!</strong>", [1 => $group_name_list, 2 => $membership_count]));
+      } else {
+        $this->assign('exclusion_group_status', E::ts("This contact is <strong>not</strong> member of any of the excluding groups (%1).", [1 => $group_name_list]));
+      }
+    }
+
+    // SECOND: exclusion activities
+    $years = $this->getExcludedYears($contact_id);
+    if (empty($years)) {
+      $this->assign('exclusion_activity_status', E::ts("This contact currently has no further individual exclusions via activity."));
+    } else {
+      $year_list = implode(', ', $years);
+      $this->assign('exclusion_activity_status', E::ts("This contact further has <strong>excluded the following years from submission : %1</strong>", [1 => $year_list]));
+    }
+
+    // FINALLY: add 'create exception' link
+    $this->assign('exclusion_activity_create', CRM_Utils_System::url('civicrm/activity/add', "action=add&reset=1&cid={$contact_id}&atype=108"));
+
+    // finally: let's add some style...
     CRM_Core_Resources::singleton()->addStyleFile('de.systopia.bpk', 'css/bmfsa.css');
 
     parent::run();
   }
 
+  /**
+   * Extract the years that have been excluded from submission
+   *  via activity for this contact
+   * @param $contact_id
+   * @return array
+   */
+  protected function getExcludedYears($contact_id) {
+    // TODO: implement
+    return range(2018,2022);
+  }
 }
